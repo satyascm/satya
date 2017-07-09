95834e25f7b0dcb4a13e00e54cf0f4ce4f7965f3
satya <satyapriya.scm@gmail.com>
2017-07-10 01:11:15 +0530

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,4 +1,3 @@
 
 #!/bin/bash
-//mail to: "satyapriya.das@cognizant.com", subject:"SUCCESS: ${currentBuild.fullDisplayName}", body: "The pipeline ${currentBuild.fullDisplayName} completed successfully."
 mail -s “Build Step Completed Successfully” satyapriya.das@cognizant.com
